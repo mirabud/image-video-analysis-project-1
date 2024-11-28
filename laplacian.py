dfe3994ea27822d31060562565da02cd79adaf38
Mira Budenova <52883647+mirabud@users.noreply.github.com>
2024-11-28 14:28:13 +0100

    Apply High-pass filtering for pre-processing

--- laplacian.py
+++ laplacian.py
@@ -1,9 +1,12 @@
 import cv2
 
 def laplacian(img):
+    # Apply High-pass filtering
     laplacian = cv2.Laplacian(img, cv2.CV_64F)
 
+    # Normalization
     laplacian = cv2.convertScaleAbs(laplacian)
+    # Threshold
     _, laplacian_thresh = cv2.threshold(laplacian, 50, 200, cv2.THRESH_BINARY)
 
     return laplacian_thresh
