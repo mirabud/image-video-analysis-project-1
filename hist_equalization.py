8ab6d1a40cd44a7916b00194b9d0457f65b7ae47
Mira Budenova <52883647+mirabud@users.noreply.github.com>
2024-11-28 13:43:38 +0100

    Delete hist_equalization.py

--- hist_equalization.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def hist_equalization(img):
-    equalized = cv2.equalizeHist(img)
-
-    _, binary_thresh = cv2.threshold(equalized, 50, 200, cv2.THRESH_BINARY)
-
-    return binary_thresh
