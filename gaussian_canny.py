4c60522732c4c032d441df7a9c41cab47c784716
Mira Budenova <52883647+mirabud@users.noreply.github.com>
2024-11-28 14:25:22 +0100

    Update gaussian_canny.py

--- gaussian_canny.py
+++ gaussian_canny.py
@@ -1,9 +1,10 @@
 import cv2
 
 def gaussian_canny(img):
+        # Apply Gaussian Blur
         img_blur = cv2.GaussianBlur(img, (5, 5), 0)
 
-    # # Apply the detector of edges Canny
+        # Apply the detector of edges Canny
         edges = cv2.Canny(img_blur, 50, 200)
         
         return edges
