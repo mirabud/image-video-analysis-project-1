163b4d07a100e6e39997ada0b89cc4c100e4698a
Mira Budenova <52883647+mirabud@users.noreply.github.com>
2024-11-28 14:26:57 +0100

    Apply Histogram equalization for pre-processing

--- hist_equalization.py
+++ hist_equalization.py
@@ -1,8 +1,10 @@
 import cv2
 
 def hist_equalization(img):
+    # Apply Histogram equalization
     equalized = cv2.equalizeHist(img)
 
+    # Get binary threshold 
     _, binary_thresh = cv2.threshold(equalized, 50, 200, cv2.THRESH_BINARY)
 
     return binary_thresh
